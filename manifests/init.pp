abd1fbd2b2f90b5ccbb107c63b73bf00010391ad
Les Aker <me@lesaker.org>
2015-06-21 15:24:39 -0700

    fix app name

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,17 +8,17 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class totalspaces (
-  $install_path = "/Users/${::boxen_user}/Applications/TotalSpaces.app"
+  $install_path = "/Users/${::boxen_user}/Applications/TotalSpaces2.app"
 ) {
   class { '::totalspaces::config': } ->
   package { 'totalspaces-halyard':
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
   } ->
-  osx_login_item { 'TotalSpaces':
+  osx_login_item { 'TotalSpaces2':
     path   => $install_path
   } ~>
-  exec { "launch TotalSpaces":
+  exec { "launch TotalSpaces2":
     command     => "/usr/bin/open '${install_path}'",
     refreshonly => true,
   }
