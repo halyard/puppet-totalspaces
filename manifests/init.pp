572e84455721bc8bdb74445644259599d4ae3fa2
Les Aker <me@lesaker.org>
2017-04-05 23:14:10 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,11 @@ class totalspaces (
   package { 'totalspaces-halyard':
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
-  } ->
-  osx_login_item { 'TotalSpaces2':
+  }
+  -> osx_login_item { 'TotalSpaces2':
     path   => $install_path
-  } ~>
-  exec { 'launch TotalSpaces2':
+  }
+  ~> exec { 'launch TotalSpaces2':
     command     => "/usr/bin/open '${install_path}'",
     refreshonly => true,
   }
